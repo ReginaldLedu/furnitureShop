0c4546dd371e3b8cef14e68de17ee515f29e8bd0
Regina Askerova <saparova.regina@yandex.ru>
2025-01-27 14:36:59 +0300

    deno

--- main.ts
+++ main.ts
@@ -3,30 +3,30 @@ import { Application, send } from 'https://deno.land/x/oak/mod.ts';
 const app = new Application();
 
 // Папка, из которой будут отдаваться статические файлы
-const STATIC_FILES_DIR = './build';
+const STATIC_FILES_DIR = './build/static';
 
 // Middleware для обслуживания статических файлов
 app.use(async (context, next) => {
   const { pathname } = context.request.url;
 
   // Проверяем, если запрашивается файл
-  if (pathname.startsWith('/static/')) {
-    // Удаляем префикс "/static/" из пути
-    const filePath = pathname.replace('/static/', '');
+  // if (pathname.startsWith('/static/')) {
+  //   // Удаляем префикс "/static/" из пути
+  //   const filePath = pathname.replace('/static/', '');
 
-    try {
-      // Отдаем файл
-      await send(context, filePath, {
-        root: STATIC_FILES_DIR,
-        index: false,
-      });
-    } catch (error) {
-      console.error(`Файл не найден: ${filePath}, error`);
-      await next();
-    }
-  } else {
-    await next();
-  }
+  //   try {
+  //     // Отдаем файл
+  //     await send(context, filePath, {
+  //       root: STATIC_FILES_DIR,
+  //       index: false,
+  //     });
+  //   } catch (error) {
+  //     console.error(`Файл не найден: ${filePath}, error`);
+  //     await next();
+  //   }
+  // } else {
+  //   await next();
+  // }
 });
 
 // Middleware для обработки всех остальных запросов
